ff6fd0eca37b2ff3dda5264360f569c7d880485d
SulTenZ <sultan.akmalghiffari@gmail.com>
2025-06-14 15:24:44 +0700

    first innit porto

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-blue-300">
+      <h1 className="text-4xl font-extrabold text-blue-700">React + Tailwind v3.4.17 + Vite Siap! 🚀</h1>
+    </div>
+  );
+}
+export default App;
