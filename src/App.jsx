bfba067284da7c9a53130d8337ac3ecc9a468a54
SulTenZ <sultan.akmalghiffari@gmail.com>
2025-08-06 21:23:52 +0700

    error fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./sections/Navbar";
 import Footer from "./sections/Footer";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
+// import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 
 function App() {
